2c38e4c4b516424b5d2fb19476dccca4da3fbc9b
abhishekpathak-agva <abhishekpathak@ddtech.in>
2023-03-09 11:54:48 +0530

    Adding files to github

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    val rectangle = Rectangle(5.0,2.0)
+    println("The perimeter is ${rectangle.perimeter}")
+}
---
+++ src/main/kotlin/Rectangle.kt
@@ -0,0 +1,5 @@
+class Rectangle(var height:Double,var length:Double):Shape() {
+    var perimeter = (height+length)*2
+}
+
+open class Shape
